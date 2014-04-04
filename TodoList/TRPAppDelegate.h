ad26ae7e178582a669ce96289e54558390cba615
ripleyaffect <t.ripley.peterson@gmail.com>
2014-04-03 20:16:31 -0400

    Initial Commit

---
+++ TodoList/TRPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TRPAppDelegate.h
+//  TodoList
+//
+//  Created by Thomas Peterson on 4/3/14.
+//  Copyright (c) 2014 Ripley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TRPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TodoList/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TodoList
+//
+//  Created by Thomas Peterson on 4/3/14.
+//  Copyright (c) 2014 Ripley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TRPAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TRPAppDelegate class]));
+    }
+}
